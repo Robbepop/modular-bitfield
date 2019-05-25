d97c18c2079afdddc560b495df8a5b1ebaf0eeb6
Robin Freyler <robin.freyler@gmail.com>
2019-05-25 14:30:41 +0200

    Add new error variant for invalid buffer lengths
    This is going to be used for the TryFrom implementation of bitfields for byte conversions.

--- src/lib.rs
+++ src/lib.rs
@@ -163,12 +163,19 @@ pub enum Error {
     /// The error is returned if a user tries to set its value to a value
     /// that is not within the range `0..4`, e.g. 5.
     OutOfBounds,
+    /// Encountered upon using `from_bytes` if too many or too few bytes have been given.
+    InvalidBufferLen,
 }
 
 impl core::fmt::Display for Error {
     fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
         match self {
-            Error::OutOfBounds => write!(f, "Encountered an out of bounds value"),
+            Error::OutOfBounds => {
+                write!(f, "Encountered an out of bounds value")
+            }
+            Error::InvalidBufferLen => {
+                write!(f, "Too many or too few bytes given to construct from bytes")
+            }
         }
     }
 }
