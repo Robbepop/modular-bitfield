fe00c6badb4e99b98e5e3c90cc03cfeb167e9494
Robin Freyler <robin.freyler@gmail.com>
2020-10-25 12:25:04 +0100

    slightly improve impls of PopBits and PushBits

--- src/private/impls.rs
+++ src/private/impls.rs
@@ -65,14 +65,11 @@ impl_specifier_for_primitive!(
 impl PopBits for u8 {
     #[inline(always)]
     fn pop_bits(&mut self, amount: u32) -> u8 {
-        let orig_bits = self.count_ones();
+        let orig_ones = self.count_ones();
         debug_assert!(0 < amount && amount <= 8);
         let res = *self & ((0x01_u16.wrapping_shl(amount)).wrapping_sub(1) as u8);
-        *self = match self.overflowing_shr(amount) {
-            (v, false) => v,
-            _ => 0,
-        };
-        debug_assert_eq!(res.count_ones() + self.count_ones(), orig_bits);
+        *self = self.checked_shr(amount).unwrap_or(0);
+        debug_assert_eq!(res.count_ones() + self.count_ones(), orig_ones);
         res
     }
 }
@@ -83,11 +80,11 @@ macro_rules! impl_push_bits {
             impl PushBits for $type {
                 #[inline(always)]
                 fn push_bits(&mut self, amount: u32, bits: u8) {
-                    let orig_bits = self.count_ones();
+                    let orig_ones = self.count_ones();
                     debug_assert!(0 < amount && amount <= 8);
-                    *self = self.wrapping_shl(amount);
-                    *self |= (bits & (0xFF >> (8 - amount))) as $type;
-                    debug_assert_eq!((bits & (0xFF >> (8 - amount))).count_ones() + orig_bits, self.count_ones());
+                    let bitmask = 0xFF >> (8 - amount as u8);
+                    *self = self.wrapping_shl(amount) | ((bits & bitmask) as $type);
+                    debug_assert_eq!((bits & bitmask).count_ones() + orig_ones, self.count_ones());
                 }
             }
         )+
@@ -101,14 +98,12 @@ macro_rules! impl_pop_bits {
             impl PopBits for $type {
                 #[inline(always)]
                 fn pop_bits(&mut self, amount: u32) -> u8 {
-                    let orig_bits = self.count_ones();
+                    let orig_ones = self.count_ones();
                     debug_assert!(0 < amount && amount <= 8);
-                    let res = (*self & (0xFF >> (8 - amount))) as u8;
-                    *self = match self.overflowing_shr(amount) {
-                        (v, false) => v,
-                        _ => 0,
-                    };
-                    debug_assert_eq!(res.count_ones() + self.count_ones(), orig_bits);
+                    let bitmask = 0xFF >> (8 - amount);
+                    let res = (*self & bitmask) as u8;
+                    *self = self.checked_shr(amount).unwrap_or(0);
+                    debug_assert_eq!(res.count_ones() + self.count_ones(), orig_ones);
                     res
                 }
             }
