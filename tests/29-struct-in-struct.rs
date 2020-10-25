521c53ef9a963fb27da761e9910a8044fd2d8e86
Robin Freyler <robin.freyler@gmail.com>
2020-10-25 22:58:19 +0100

    fix unit test for bitfield structs in bitfield structs

--- tests/29-struct-in-struct.rs
+++ tests/29-struct-in-struct.rs
@@ -1,19 +1,21 @@
 use modular_bitfield::prelude::*;
 
 #[bitfield(specifier = true)]
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
 pub struct Header {
     a: B2,
     b: B3,
 }
 
 #[bitfield]
+#[derive(Debug, PartialEq, Eq)]
 pub struct Base {
     pub header: Header,
     pub rest: B3,
 }
 
 fn main() {
-    let base = Base::new();
+    let mut base = Base::new();
     assert_eq!(base.header(), Header::new());
     let h = Header::new().with_a(1).with_b(2);
     base.set_header(h);
