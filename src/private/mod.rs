3064363b30e0b2ec9a6ff33dad6f8b210cc831d0
Robin Freyler <robin.freyler@gmail.com>
2020-10-25 14:03:18 +0100

    add PushBuffer and PopBuffer abstractions

--- src/private/mod.rs
+++ src/private/mod.rs
@@ -1,6 +1,7 @@
 pub mod checks;
 mod impls;
 mod proc;
+mod push_pop;
 mod traits;
 mod utils;
 
@@ -8,6 +9,10 @@ pub use self::{
     proc::{
         read_specifier,
         write_specifier,
+    },
+    push_pop::{
+        PopBuffer,
+        PushBuffer,
     },
     traits::{
         FromBits,
---
+++ src/private/push_pop.rs
@@ -0,0 +1,93 @@
+use crate::private::{checks::private::Sealed, PushBits, PopBits};
+
+/// A bit buffer that allows to pop bits from it.
+pub struct PopBuffer<T> {
+    bytes: T,
+}
+
+impl<T> PopBuffer<T> {
+    /// Creates a new pop buffer from the given bytes.
+    #[inline]
+    pub(super) fn from_bytes(bytes: T) -> Self {
+        Self { bytes }
+    }
+}
+
+impl Sealed for PopBuffer<u8> {}
+
+impl PopBits for PopBuffer<u8> {
+    #[inline(always)]
+    fn pop_bits(&mut self, amount: u32) -> u8 {
+        let Self { bytes } = self;
+        let orig_ones = bytes.count_ones();
+        debug_assert!(0 < amount && amount <= 8);
+        let res = *bytes & ((0x01_u16.wrapping_shl(amount)).wrapping_sub(1) as u8);
+        *bytes = bytes.checked_shr(amount).unwrap_or(0);
+        debug_assert_eq!(res.count_ones() + bytes.count_ones(), orig_ones);
+        res
+    }
+}
+
+macro_rules! impl_pop_bits {
+    ( $($type:ty),+ ) => {
+        $(
+            impl Sealed for PopBuffer<$type> {}
+
+            impl PopBits for PopBuffer<$type> {
+                #[inline(always)]
+                fn pop_bits(&mut self, amount: u32) -> u8 {
+                    let Self { bytes } = self;
+                    let orig_ones = bytes.count_ones();
+                    debug_assert!(0 < amount && amount <= 8);
+                    let bitmask = 0xFF >> (8 - amount);
+                    let res = (*bytes & bitmask) as u8;
+                    *bytes = bytes.checked_shr(amount).unwrap_or(0);
+                    debug_assert_eq!(res.count_ones() + bytes.count_ones(), orig_ones);
+                    res
+                }
+            }
+        )+
+    };
+}
+impl_pop_bits!(u16, u32, u64, u128);
+
+/// A bit buffer that allows to push bits onto it.
+pub struct PushBuffer<T> {
+    bytes: T,
+}
+
+impl<T> PushBuffer<T> {
+    /// Returns the underlying bytes of the push buffer.
+    #[inline]
+    pub(super) fn into_bytes(self) -> T {
+        self.bytes
+    }
+}
+
+macro_rules! impl_push_bits {
+    ( $($type:ty),+ ) => {
+        $(
+            impl Sealed for PushBuffer<$type> {}
+
+            impl Default for PushBuffer<$type> {
+                #[inline]
+                fn default() -> Self {
+                    Self { bytes: <$type as Default>::default() }
+                }
+            }
+
+            impl PushBits for PushBuffer<$type> {
+                #[inline(always)]
+                fn push_bits(&mut self, amount: u32, bits: u8) {
+                    let Self { bytes } = self;
+                    let orig_ones = bytes.count_ones();
+                    debug_assert!(0 < amount && amount <= 8);
+                    let bitmask = 0xFF >> (8 - amount as u8);
+                    *bytes = bytes.wrapping_shl(amount) | ((bits & bitmask) as $type);
+                    debug_assert_eq!((bits & bitmask).count_ones() + orig_ones, bytes.count_ones());
+                }
+            }
+        )+
+    }
+}
+impl_push_bits!(u8, u16, u32, u64, u128);
