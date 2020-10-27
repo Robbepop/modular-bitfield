9fcaca2bc3dc098491343b1ae190dcbf168e88f5
Robin Freyler <robin.freyler@gmail.com>
2020-10-27 19:42:56 +0100

    [tests] add some expected failure outputs for the new failing tests

--- tests/filled-param/valid-bitfield-specifier-2.rs
+++ tests/filled-param/valid-bitfield-specifier-2.rs
@@ -2,7 +2,7 @@ use modular_bitfield::prelude::*;
 
 // The bitfield has 24 bits and therefore is filled.
 #[bitfield(specifier = true, filled = true)]
-pub struct UnfilledSpecifier {
+pub struct FilledSpecifier {
     a: B8,
     b: u16,
 }
