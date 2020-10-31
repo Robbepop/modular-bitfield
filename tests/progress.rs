5570f77161d7f908ace638445b0e996865cc4f51
Robin Freyler <robin.freyler@gmail.com>
2020-10-31 21:06:15 +0100

    [tests] remove deprecated tests

--- tests/progress.rs
+++ tests/progress.rs
@@ -32,9 +32,6 @@ fn tests() {
     t.compile_fail("tests/27-invalid-union-specifier.rs");
     t.pass("tests/28-single-bit-enum.rs");
     t.pass("tests/regressions/regression-issue-8.rs");
-    t.pass("tests/29-struct-in-struct.rs");
-    t.compile_fail("tests/30-out-of-bounds-specifier.rs");
-    t.pass("tests/31-unfilled-from-bytes.rs");
     t.pass("tests/regressions/deny_elided_lifetime.rs");
     t.compile_fail("tests/regressions/invalid_bits_field_attr.rs");
 
